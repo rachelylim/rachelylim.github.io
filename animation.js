075069269046d740727eff674506d50b54a2cf0d
rachelylim <rachel.y.lim@gmail.com>
2015-09-24 15:28:07 -0400

    add animation js file. add link to js file on all pages.

---
+++ animation.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('.fa').hover(
+    function() {
+        $(this).css('color', '#A6AAB1')
+    }, function() {
+        $(this).css('color', '')
+    });
+
+	// $('div #blog').click(function() {
+	// 	$(this 'section').slideToggle('slow');
+	// });
+});
